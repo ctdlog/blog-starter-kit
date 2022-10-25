b8db7a4775e046d268db6c94513392f97e321d55
ctd <qpflapffhs76@gmail.com>
2022-10-25 17:09:31 +0900

    💄 update PostCard style

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -4,8 +4,17 @@ import { PostType } from '../interfaces/post';
 const PostCard = ({ postInfo }: { postInfo: PostType }) => {
   const { slug, title, date } = postInfo;
   return (
-    <Link href={`/${slug}`} style={{ border: '1px solid gray' }}>
-      <div>
+    <Link href={`/${slug}`}>
+      <div
+        style={{
+          width: '400px',
+          border: '1px solid gray',
+          borderRadius: '4px',
+          padding: '12px',
+          margin: '8px 0',
+          cursor: 'pointer',
+        }}
+      >
         {title} - {date}
       </div>
     </Link>
